1099a6d94babf9eb0bdebd1c23d90818602a7f3e
Vidsneezes <moraleszez@gmail.com>
2017-08-23 17:48:03 -0600

    hooked video games view activity page, added a basic json reader

--- app/src/main/java/com/moraleszez/videogamerr/videogamereviewer/VideoGamesViewActivity.java
+++ app/src/main/java/com/moraleszez/videogamerr/videogamereviewer/VideoGamesViewActivity.java
@@ -1,13 +1,29 @@
 package com.moraleszez.videogamerr.videogamereviewer;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.JsonReader;
 import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import com.moraleszez.videogamerr.backendcore.VideoGame;
+
+import org.json.JSONObject;
 
 public class VideoGamesViewActivity extends AppCompatActivity {
+
+    AppBarLayout appBarLayoutRoot;
+    TextView gamesListView;
+
+    List<VideoGame> videoGames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +31,10 @@ public class VideoGamesViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video_games_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        appBarLayoutRoot = (AppBarLayout) findViewById(R.id.app_bar);
+        gamesListView = (TextView) findViewById(R.id.videogameslist);
+        toolbar.setTitle("Games Title");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +44,18 @@ public class VideoGamesViewActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void LoadGamesToList()
+    {
+        JsonReader jsonReader = new JsonReader(new InputStreamReader( getAssets().open("videogames.json"));
+        jsonReader.beginObject();
+
+        while(jsonReader.hasNext())
+        {
+            String name = jsonReader.nextName();
+        }
+        jsonReader.endObject();
+
     }
 }
